8a4b6acb860f154555db6145d7928cda6fc88054
Lukasz Sobczak <153393240+LukaszMateuszSobczak@users.noreply.github.com>
2025-05-06 19:20:26 +0200

    fetched data

--- script/main.js
+++ script/main.js
@@ -6,4 +6,39 @@ const tweetBtn = document.querySelector('#tweet-btn');
 tweetBtn.addEventListener('click', () => {
     console.log(tweetInput.value);
 });
-~~
+
+const getFeedHtml = () => {
+    let feedHtml = '';
+    tweetsData.forEach((tweet) => {
+        feedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+        </div>
+        `;
+    });
+    return feedHtml;
+};
+
+const renderHtml = (elementToRender, destinationElement) => {
+    destinationElement.innerHTML += elementToRender;
+};
+
+console.log(getFeedHtml());
+renderHtml(getFeedHtml(), document.querySelector('#feed'));
