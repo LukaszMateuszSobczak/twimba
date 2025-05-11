5a816efd9b29e1f626f33ec079fe26db39e472bd
Lukasz Sobczak <153393240+LukaszMateuszSobczak@users.noreply.github.com>
2025-05-11 16:03:26 +0200

    added icon to reaction, like handler

--- script/main.js
+++ script/main.js
@@ -19,12 +19,18 @@ const getFeedHtml = () => {
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
+                        <i class="fa-regular fa-comment-dots" 
+                            data-reply-icon="${tweet.uuid}"></i>
                             ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
+                        <i class="fa-solid fa-heart" 
+                            data-like-icon="${tweet.uuid}"></i>
                             ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
+                        <i class="fa-solid fa-retweet" 
+                        data-share-icon="${tweet.uuid}"></i>
                             ${tweet.retweets}
                         </span>
                     </div>   
@@ -40,5 +46,30 @@ const renderHtml = (elementToRender, destinationElement) => {
     destinationElement.innerHTML += elementToRender;
 };
 
-console.log(getFeedHtml());
+const reRenderElement = (elementLocation, elementContent) => {
+    elementLocation.nextSibling.textContent = elementContent;
+};
+
+const handleLikeClick = (uuid) => {
+    tweetsData.forEach((tweet) => {
+        if (tweet.uuid === uuid) {
+            tweet.likes += 1;
+            reRenderElement(
+                document.querySelector(`[data-like-icon="${tweet.uuid}"]`),
+                tweet.likes
+            );
+        }
+    });
+};
+
+document.addEventListener('click', (event) => {
+    if (event.target.dataset.replyIcon) {
+        console.log(event.target.dataset.replyIcon, 'reply');
+    } else if (event.target.dataset.likeIcon) {
+        handleLikeClick(event.target.dataset.likeIcon);
+    } else if (event.target.dataset.shareIcon) {
+        console.log(event.target.dataset.shareIcon, 'share');
+    }
+});
+
 renderHtml(getFeedHtml(), document.querySelector('#feed'));
